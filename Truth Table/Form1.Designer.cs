8f3833d5d93cb97a13b5e660b4b8e2201a3c6230
Daniel Seavers <daniel.p.seavers@gmail.com>
2020-06-01 11:21:52 +0100

    Developed program

--- Truth Table/Form1.Designer.cs
+++ Truth Table/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBX_Operators = new System.Windows.Forms.ComboBox();
+            this.BTN_Table = new System.Windows.Forms.Button();
+            this.TBL_TruthTable = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Truth Table Generator";
+            // 
+            // CBX_Operators
+            // 
+            this.CBX_Operators.FormattingEnabled = true;
+            this.CBX_Operators.Items.AddRange(new object[] {
+            "AND",
+            "OR",
+            "NAND",
+            "NOR",
+            "XOR"});
+            this.CBX_Operators.Location = new System.Drawing.Point(18, 120);
+            this.CBX_Operators.Name = "CBX_Operators";
+            this.CBX_Operators.Size = new System.Drawing.Size(298, 21);
+            this.CBX_Operators.TabIndex = 2;
+            // 
+            // BTN_Table
+            // 
+            this.BTN_Table.Location = new System.Drawing.Point(12, 146);
+            this.BTN_Table.Name = "BTN_Table";
+            this.BTN_Table.Size = new System.Drawing.Size(305, 37);
+            this.BTN_Table.TabIndex = 4;
+            this.BTN_Table.Text = "Generate Table";
+            this.BTN_Table.UseVisualStyleBackColor = true;
+            this.BTN_Table.Click += new System.EventHandler(this.BTN_Table_Click);
+            // 
+            // TBL_TruthTable
+            // 
+            this.TBL_TruthTable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.TBL_TruthTable.ColumnCount = 3;
+            this.TBL_TruthTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.TBL_TruthTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TBL_TruthTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TBL_TruthTable.Location = new System.Drawing.Point(12, 189);
+            this.TBL_TruthTable.Name = "TBL_TruthTable";
+            this.TBL_TruthTable.RowCount = 3;
+            this.TBL_TruthTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.TBL_TruthTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TBL_TruthTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TBL_TruthTable.Size = new System.Drawing.Size(305, 148);
+            this.TBL_TruthTable.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(329, 354);
+            this.Controls.Add(this.TBL_TruthTable);
+            this.Controls.Add(this.BTN_Table);
+            this.Controls.Add(this.CBX_Operators);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CBX_Operators;
+        private System.Windows.Forms.Button BTN_Table;
+        private System.Windows.Forms.TableLayoutPanel TBL_TruthTable;
     }
 }
 
--- Truth Table/Form1.cs
+++ Truth Table/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Truth_Table
 {
     public partial class Form1 : Form
@@ -15,6 +14,65 @@ namespace Truth_Table
         public Form1()
         {
             InitializeComponent();
+        }
+        private void BTN_Table_Click(object sender, EventArgs e)
+        {
+            TBL_TruthTable.Controls.Clear();
+            TextBox tbx_1 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_1, 1, 0);
+            TextBox tbx_2 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_2, 2, 0);
+            TextBox tbx_3 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_3, 0, 1);
+            TextBox tbx_4 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_4, 1, 1);
+            TextBox tbx_5 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_5, 2, 1);
+            TextBox tbx_6 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_6, 0, 2);
+            TextBox tbx_7 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_7, 1, 2);
+            TextBox tbx_8 = new TextBox();
+            TBL_TruthTable.Controls.Add(tbx_8, 2, 2);
+            tbx_1.Text = "1";
+            tbx_2.Text = "0";
+            tbx_3.Text = "1";
+            tbx_6.Text = "0";
+            if (CBX_Operators.Text == "AND")
+            {
+                tbx_4.Text = "1";
+                tbx_5.Text = "0";
+                tbx_7.Text = "0";
+                tbx_8.Text = "0";
+            }
+            if (CBX_Operators.Text == "OR")
+            {
+                tbx_4.Text = "1";
+                tbx_5.Text = "1";
+                tbx_7.Text = "1";
+                tbx_8.Text = "0";
+            }
+            if (CBX_Operators.Text == "NAND")
+            {
+                tbx_4.Text = "0";
+                tbx_5.Text = "1";
+                tbx_7.Text = "1";
+                tbx_8.Text = "1";
+            }
+            if (CBX_Operators.Text == "NOR")
+            {
+                tbx_4.Text = "0";
+                tbx_5.Text = "0";
+                tbx_7.Text = "0";
+                tbx_8.Text = "1";
+            }
+            if (CBX_Operators.Text == "XOR")
+            {
+                tbx_4.Text = "0";
+                tbx_5.Text = "1";
+                tbx_7.Text = "1";
+                tbx_8.Text = "0";
+            }
         }
     }
 }
